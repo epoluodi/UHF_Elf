4c058050b16db1ee1fb2ee3607de710cf6712023
epoluodi <12163687@qq.com>
2016-04-12 20:51:35 +0800

    增加扫描窍界面

---
+++ app/src/main/java/kaiqiaole/com/uhf_elf/LabelBindingActivity.java
@@ -0,0 +1,13 @@
+package kaiqiaole.com.uhf_elf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LabelBindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_label_binding);
+    }
+}
